bad3d9fbe33af272524c31958a5ec7329de065d3
Adel Qunber <adelqunber@gmail.com>
2026-05-10 18:54:48 +0300

    Header for GUI.c

--- GUI.h
+++ GUI.h
@@ -3,6 +3,9 @@
 
 #include "graph.h"
 
+// Header for Milestone2 and Milestone3
+
 void show_graph_static(const Graph* graph);
+void show_graph_animation(const Graph* graph, const int* path, int path_length);
 
 #endif

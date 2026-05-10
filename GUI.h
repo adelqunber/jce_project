4adbb02c9dc794688a02b0084c7fe1156a22e99d
adelqunber <adelqunber@gmail.com>
2026-05-10 17:28:04 +0300

    Delete GUI.h

--- GUI.h
+++
@@ -1,9 +0,0 @@
-#ifndef GUI_H
-#define GUI_H
-
-#include "graph.h"
-
-void show_graph_static(const Graph* graph);
-void show_graph_animation(const Graph* graph, const int* path, int path_length);
-
-#endif
